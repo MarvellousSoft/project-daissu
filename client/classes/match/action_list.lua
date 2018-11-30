c300d8a51866f0324cb7314d27199e6b02f73bbc
rilifon <pudimdefrutas@gmail.com>
2018-11-30 13:27:47 -0200

    Better indication of current action

--- client/classes/match/action_list.lua
+++ client/classes/match/action_list.lua
@@ -37,7 +37,11 @@ function ActionList:init(pos,actions,players)
     end
 
     --Y position for the arrows
-    self.arrow_y = self.pos.y + die_h/2 - image:getHeight()/2
+    self.focused_arrow_scale = 1
+    self.focused_arrow_y = self.pos.y + die_h/2 - image:getHeight()/2
+    self.unfocused_arrow_scale = .8
+    self.unfocused_arrow_y = self.pos.y + die_h/2 - image:getHeight()*self.unfocused_arrow_scale/2
+    self.unfocused_offset = image:getWidth()*(self.focused_arrow_scale-self.unfocused_arrow_scale)
 
     self.grey_color = Color.new(150,150,150) --For dice that already had their action done
 
@@ -48,17 +52,29 @@ function ActionList:draw()
     local g = love.graphics
     local dx = DieHelper.getDimensions() + 2 * self.gap + IMG.next_action_grey:getWidth() --How far apart each action is
     local x = self.pos.x
+    local arrow_alpha, arrow_y
     for i,die in ipairs(self.dice) do
         local image
+        local offset = self.unfocused_offset
+        arrow_y = self.unfocused_arrow_y
+        scale = self.unfocused_arrow_scale
         if i < self.current_action then
             die:setColor(self.grey_color)
             image = IMG["next_action_grey"]
+            arrow_alpha = 150
         else
             image = IMG["next_action_"..self.colors[self.players[i]]]
+            arrow_alpha = 160
+            if i == self.current_action then
+                arrow_y = self.focused_arrow_y
+                scale = self.focused_arrow_scale
+                arrow_alpha = 255
+                offset = math.sin(10*love.timer.getTime())*3
+            end
         end
         --Draw correspondent arrow for this action
-        g.setColor(255,255,255,255)
-        g.draw(image, x, self.arrow_y)
+        g.setColor(255,255,255,arrow_alpha)
+        g.draw(image, x + offset, arrow_y, nil, scale)
         --Draw die representing this action
         die:draw()
         --Update arrou position
