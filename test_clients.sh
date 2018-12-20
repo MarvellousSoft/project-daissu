68e4b19101f1c9c17030a44e3c45920f0d9ecc92
Yan Couto <yancouto@gmail.com>
2018-12-20 17:47:51 -0200

    Improvements to the test clients

--- test_clients.sh
+++ test_clients.sh
@@ -8,6 +8,6 @@ ROOM=`uuidgen`
 ((COUNT--))
 
 for i in `seq 1 $COUNT`; do
-    love client/ --host=localhost --char=melee --auto-connect --room=$ROOM --auto-ready=0.5 "${@:2}" &>/dev/null &
+    love client/ --host=localhost --char=melee --auto-connect --room=$ROOM --auto-ready=0.5 "${@:2}" &>client$i.log &
 done
-love client/ --host=localhost --char=ranged --auto-connect --room=$ROOM --auto-ready=0.5 "${@:2}" &
+love client/ --host=localhost --char=ranged --auto-connect --room=$ROOM --auto-ready=0.5 "${@:2}" | tee client$COUNT.log
